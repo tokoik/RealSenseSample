f6ec9b74191adc0b6f1e9b1df3b4435b7be68b8a
Kohe Tokoi <tokoi@wakayama-u.ac.jp>
2023-10-09 18:29:04 +0900

    draw by triangle mesh

--- Assets/Scripts/TriangleMeshRenderer.cs
+++ Assets/Scripts/TriangleMeshRenderer.cs
@@ -37,6 +37,47 @@ public class TriangleMeshRenderer : MonoBehaviour
     Source.OnNewSample += OnNewSample;
   }
 
+  private static int[] CreateTriangleMeshIndex(int slices, int stacks)
+  {
+    // 三角形の頂点の数
+    int vertices = slices * stacks * 6;
+
+    // 三角形のデータ
+    int[] indeces = new int[vertices];
+
+    // 三角形の頂点番号を求める
+    for (int j = 0; j < stacks; ++j)
+    {
+      // 各段の左端の下側の頂点番号の格納先
+      int f0 = j * slices * 6;
+
+      // 各段の左端の下側の頂点の頂点番号
+      int p0 = j * (slices + 1);
+
+      for (int i = 0; i < slices; ++i)
+      {
+        // 左から i 番目の四角形の左下の頂点番号の格納先
+        int fi = f0 + i * 6;
+
+        // 左から i 番目の四角形の左下の頂点番号
+        int pi = p0 + i;
+
+        // １つ目の三角形の頂点番号
+        indeces[fi + 0] = pi;
+        indeces[fi + 1] = pi + 1;
+        indeces[fi + 2] = pi + slices + 1;
+
+        // ２つ目の三角形の頂点番号
+        indeces[fi + 3] = pi + slices + 2;
+        indeces[fi + 4] = pi + slices + 1;
+        indeces[fi + 5] = pi + 1;
+      }
+    }
+
+    // 作成したインデックスを返す
+    return indeces;
+  }
+
   private void ResetMesh(int width, int height)
   {
     Assert.IsTrue(SystemInfo.SupportsTextureFormat(TextureFormat.RGFloat));
@@ -57,9 +98,10 @@ public class TriangleMeshRenderer : MonoBehaviour
 
     vertices = new Vector3[width * height];
 
-    var indices = new int[vertices.Length];
-    for (int i = 0; i < vertices.Length; i++)
-      indices[i] = i;
+    //var indices = new int[vertices.Length];
+    //for (int i = 0; i < vertices.Length; i++)
+    //  indices[i] = i;
+    var indices = CreateTriangleMeshIndex(width - 1, height - 1);
 
     mesh.MarkDynamic();
     mesh.vertices = vertices;
@@ -77,7 +119,8 @@ public class TriangleMeshRenderer : MonoBehaviour
 
     mesh.uv = uvs;
 
-    mesh.SetIndices(indices, MeshTopology.Points, 0, false);
+    //mesh.SetIndices(indices, MeshTopology.Points, 0, false);
+    mesh.SetIndices(indices, MeshTopology.Triangles, 0, false);
     mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 10f);
 
     GetComponent<MeshFilter>().sharedMesh = mesh;
